1b5840e883c01975664fac05badf155c0898b95b
jvgiv <jvgiv12@gmail.com>
2019-04-23 19:38:54 -0400

    Post works

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -8,7 +8,7 @@ const router = express.Router()
 
 router.post('/', async (req, res) => {
     try {
-        const posts = await Posts.insert(req.post);
+        const posts = await Posts.insert(req.body);
         res.status(200).json(posts)
     } catch (error) {
         console.log(error);
